dc4622ef2e127aa3c2aa849c90662105f8ac5c8a
Stephen T. Sagarino Jr <stsagarino@vauldex.com>
2025-10-07 08:30:43 +0800

    refactor: deprecates all the `SNKDataRequest.ContentType`

--- Sources/SwiftNetworkKit/Extensions/SNKDataRequestExtension.swift
+++ Sources/SwiftNetworkKit/Extensions/SNKDataRequestExtension.swift
@@ -6,6 +6,11 @@
 //
 
 extension SNKDataRequest {
+    @available(
+        *,
+        deprecated,
+        message: "Use SwiftNetworkKit.ContentType instead"
+    )
     internal struct ContentType {
         public static let json = "application/json"
         public static let formURLEncoded = "application/x-www-form-urlencoded"
@@ -20,16 +25,31 @@ extension SNKDataRequest {
         public static let applicationOctetStream = "application/octet-stream"
     }
 
+    @available(
+        *,
+        deprecated,
+        message: "Use .contentType(.json) instead"
+    )
     /// Sets the Content-Type header to application/json
     public func jsonContentType() -> SNKDataRequest {
         return self.addHeader("Content-Type", value: ContentType.json)
     }
 
+    @available(
+        *,
+        deprecated,
+        message: "Use .contentType(.formURLEncoded) instead"
+    )
     /// Sets the Content-Type header to application/x-www-form-urlencoded
     public func formContentType() -> SNKDataRequest {
         return self.addHeader("Content-Type", value: ContentType.formURLEncoded)
     }
 
+    @available(
+        *,
+        deprecated,
+        message: "Use .contentType(.multipartFormData) instead"
+    )
     /// Sets the Content-Type header to multipart/form-data
     public func multipartContentType() -> SNKDataRequest {
         return self.addHeader(
@@ -38,36 +58,71 @@ extension SNKDataRequest {
         )
     }
 
+    @available(
+        *,
+        deprecated,
+        message: "Use .contentType(.textPlain) instead"
+    )
     /// Sets the Content-Type header to text/plain
     public func textContentType() -> SNKDataRequest {
         return self.addHeader("Content-Type", value: ContentType.textPlain)
     }
 
+    @available(
+        *,
+        deprecated,
+        message: "Use .contentType(.textHTML) instead"
+    )
     /// Sets the Content-Type header to text/html
     public func htmlContentType() -> SNKDataRequest {
         return self.addHeader("Content-Type", value: ContentType.textHTML)
     }
 
+    @available(
+        *,
+        deprecated,
+        message: "Use .contentType(.applicationXML) instead"
+    )
     /// Sets the Content-Type header to application/xml
     public func xmlContentType() -> SNKDataRequest {
         return self.addHeader("Content-Type", value: ContentType.applicationXML)
     }
 
+    @available(
+        *,
+        deprecated,
+        message: "Use .contentType(.applicationPDF) instead"
+    )
     /// Sets the Content-Type header to application/pdf
     public func pdfContentType() -> SNKDataRequest {
         return self.addHeader("Content-Type", value: ContentType.applicationPDF)
     }
 
+    @available(
+        *,
+        deprecated,
+        message: "Use .contentType(.imagePNG) instead"
+    )
     /// Sets the Content-Type header to image/png
     public func pngContentType() -> SNKDataRequest {
         return self.addHeader("Content-Type", value: ContentType.imagePNG)
     }
 
+    @available(
+        *,
+        deprecated,
+        message: "Use .contentType(.imageJPEG) instead"
+    )
     /// Sets the Content-Type header to image/jpeg
     public func jpegContentType() -> SNKDataRequest {
         return self.addHeader("Content-Type", value: ContentType.imageJPEG)
     }
 
+    @available(
+        *,
+        deprecated,
+        message: "Use .contentType(.applicationOctetStream) instead"
+    )
     /// Sets the Content-Type header to application/octet-stream
     public func binaryContentType() -> SNKDataRequest {
         return self.addHeader(
